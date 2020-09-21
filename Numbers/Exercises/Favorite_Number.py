10dc49b565a9002e3c477c68046d5d1c6c82ac96
Ahsan Khan <Ahsank0120@gmail.com>
2020-09-21 17:41:11 -0400

    Working with numbers (int, float, etc.)

---
+++ Numbers/Exercises/Favorite_Number.py
@@ -0,0 +1,6 @@
+# Name: Ahsan Khan
+# Date: 09/15/2020
+# Description: Printing out the favrorite number from the variable.
+
+favorite_num = 20
+print(f"Your favorite number is, {favorite_num}")
---
+++ Numbers/Numbers.py
@@ -0,0 +1,41 @@
+# Name: Ahsan Khan
+# Date: 09/15/2020
+# Description: Showing arithmetic operators
+
+# Arithmetic Operators Add (+), Subtract (-), Multiply (*), Divide (/)
+print(2 + 3)
+print(3 - 2)
+print(2 * 3)
+print(3 / 2)
+
+# Integeral Division and Exponents, x^2 will be represented as (x ** 2). 
+print(3 ** 2)
+print(3 ** 3)
+print(10 ** 6)
+print(3 // 2)
+
+# Operations
+print(2 + 3*4)
+print((2 + 3) * 4)
+
+#Floats
+print(0.1 + 0.1)
+print(0.2 + 0.2)
+print(2 * 0.1)
+print(2 * 0.2)
+
+# Integers & Floats
+print(1 + 2.0)
+print(2 * 3.0)
+print(3.0 ** 2)
+
+# Numbers with variables
+universe_age = 14_000_000_000
+print(universe_age)
+
+# Multiple Assignment
+x, y, z = 0, 0, 0
+
+# CONSTANTS
+MAX_CONNECTIONS = 5000
+print(MAX_CONNECTIONS)
