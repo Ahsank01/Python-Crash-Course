60d2347d71bd06ed6e7d1cb6a4e326427d7dbf60
Ahsan Khan <Ahsank0120@gmail.com>
2020-10-06 19:03:41 -0400

    Add files via upload

--- User Input and While loop/Introducing_while_loops.py
+++ User Input and While loop/Introducing_while_loops.py
@@ -57,3 +57,57 @@ while current_number < 10:
 		continue
 
 	print(current_number)
+
+# ================================================================== #
+
+# Use lists with while loops
+
+print()
+unconfirmed_users = ['ahsank20', 'ak231', 'koke21']
+confirmed_users = []
+
+while unconfirmed_users:
+	current_user = unconfirmed_users.pop()
+	print(f"Verifying user: {current_user}.")
+
+	confirmed_users.append(current_user)
+
+print()
+
+for users in confirmed_users:
+	print(f"Verified User: {users}")
+
+# ================================================================== #
+
+# Use while loop to remove all instances of specific values from a list
+
+my_list = ['cat','dog', 'tiger', 'cat', 'cat','elephant', 'cat']
+print(my_list)
+
+while 'cat' in my_list:
+	my_list.remove('cat')
+
+print(my_list)
+
+# ================================================================== #
+
+# Use dictionary with user input and while loop
+responses = {}
+
+polling_actice = True
+
+while polling_actice:
+	name = input("\nWhat is your name? ")
+	response = input("Which mountain would you like to climb someday? ")
+
+	responses[name] = response
+
+	repeat = input("Would you like to let another person respond? (Y/N) ")
+	if repeat == 'n':
+		polling_actice = False
+
+print("\nPoll Results\n")
+for name, response in responses.items():
+	print(f"{name} would like to climb {response}")
+
+print(responses)
