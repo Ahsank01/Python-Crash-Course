f5bfa3a084fe50f1f4ea39c9e0d8425cf85a98f2
Ahsan Khan <Ahsank0120@gmail.com>
2020-10-29 15:06:04 -0400

    Classes - OOP

---
+++ Classes/The_Python_Standary_Library.py
@@ -0,0 +1,9 @@
+# Name: Ahsan Khan
+# Date: 10/28/20
+# Description: Start working with Python built-in modules
+
+from random import choice
+
+players = ['ahsan', 'khan', 'ahmed']
+first_up = choice(players)
+print(first_up)
